2c33e889fc5f93d19950f29a7c11ac70e2a0da62
chenhefu <chenhefu@guazi.com>
2023-10-17 14:34:52 +0800

    first init

---
+++ src/main/java/com/deepseahalo/codebackuptool/CodeBackupToolApplication.java
@@ -0,0 +1,13 @@
+package com.deepseahalo.codebackuptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBackupToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBackupToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepseahalo/codebackuptool/CodeBackupToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepseahalo.codebackuptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBackupToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
